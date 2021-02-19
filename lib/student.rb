b36e06abeb10abec694c6091bc1109903fe2a99c
Maxine <maxinemeurer@yahoo.com>
2021-02-19 10:44:03 -0500

    commit tree 8a740daa4a2888acb36d66c48c35132e8b40432e

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+class Student < User
+
+    attr_accessor :first_name, :last_name
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(fact)
+        @knowledge << fact
+    end
+
+    def knowledge
+        @knowledge
+    end
 
 end
